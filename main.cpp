9bc7e68607aa408e34bcaedd5c85006e3f2d5447
Vasilii Pochkaenko <vassili.pochkaenko@vitec.com>
2022-11-22 19:28:16 +0700

    feat: implement std::basic_streambuf for stdin

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-#include <string.h>
+#include <ostream>
 #include <streambuf>
 #include <iostream>
+#include <string>
 #include <unistd.h>
 
 using std::basic_streambuf;
-using std::endl;
-using std::ostream;
+using std::istream;
 using std::streamsize;
+using std::string;
 
 const int StdOutFD = 1;
 const streamsize StdOutBufferSize = 10;
@@ -51,14 +52,41 @@ private:
 	CharT _buffer[StdOutBufferSize + 1];
 };
 
+const int StdInFD = 0;
+const streamsize StdInBufferSize = 10;
+
+template < class CharT>
+class StdInBuf : public std::basic_streambuf<CharT> {
+public:
+	StdInBuf() : std::basic_streambuf<CharT>() {
+		Base::setg(_buffer, _buffer, _buffer);
+	}
+protected:
+	typedef std::basic_streambuf<CharT> Base;
+	typedef typename Base::int_type int_type;
+	typedef typename Base::char_type char_type;
+	typedef typename Base::traits_type traits_type;
+	typedef typename Base::pos_type pos_type;
+	typedef typename Base::off_type off_type;
+	virtual int_type underflow() {
+		streamsize n = read(StdInFD, _buffer, StdInBufferSize);
+		if(n > 0) {
+			Base::setg(_buffer, _buffer, _buffer + n);
+			return traits_type::to_int_type(_buffer[0]);
+		} else {
+			Base::setg(_buffer, _buffer, _buffer);
+			return traits_type::eof();
+		}
+	}
+private:
+	CharT _buffer[StdInBufferSize];
+};
+
 int main() {
-	StdOutBuf<char> ob;
-	ostream cout(&ob);
-	cout << "Hello";
-	cout << endl;
-	cout << "world\n";
-	cout << endl;
-	cout << 1;
-	cout << "text more 10 characters";
+	StdInBuf<char> ib;
+	istream cin(&ib);
+	string s, s2;
+	int i;
+	cin >> s >> s2 >> i;
 	return 0;
 }
